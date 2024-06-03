0ace1b86d93e97fc1959e1f0171994991c4f8d60
leonardo-johnc <leojohndoe94gmail.com>
2024-06-03 15:29:34 -0600

    My first JS

---
+++ Getting-Started-With-JavaScript-main/Getting Started with JavaScript - Tutorial/tny_script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: JC Leonardo
+   Date: 06/03/2024
+
+*/
+
+//Display the current date & time
+document.getElementById("dateNow").innerHTML = "m/d/y<br />h:m:s";
+
+//Display the time left until New Year's Eve//
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
